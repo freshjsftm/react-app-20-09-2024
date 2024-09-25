6552e1fd7dc97633bafb120a47fa0d036d8cd7df
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-25 18:57:49 +0300

    Add changeColor

--- src/components/ToggleText/ToggleText.jsx
+++ src/components/ToggleText/ToggleText.jsx
@@ -1,23 +1,40 @@
 import { useState } from 'react';
+import Icon from '@mdi/react';
+import {
+  mdiArrowDownDropCircleOutline,
+  mdiArrowUpDropCircleOutline,
+} from '@mdi/js';
+import { getRandomColor } from '../../utils';
 
 const ToggleText = () => {
+  const [color, setColor] = useState('maroon');
   const [close, setClose] = useState(true);
-  // close - змінна яка зберігає булеве значення
-  // setClose - функція, яка змінює значення змінної close
   const changeClose = () => {
     setClose(!close);
   };
+  const changeColor = () => {
+    setColor(getRandomColor());
+  };
   return (
-    <div>
+    <div style={{ backgroundColor: color }} onClick={changeColor}>
       <h2>
-        Title <button onClick={changeClose}>{close ? 'hide' : 'open'}</button>
+        Title
+        <span onClick={changeClose}>
+          {close ? (
+            <Icon path={mdiArrowUpDropCircleOutline} size={1} color="pink" />
+          ) : (
+            <Icon path={mdiArrowDownDropCircleOutline} size={1} color="pink" />
+          )}
+        </span>
       </h2>
-      {close && <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, sequi?
-        Laboriosam impedit esse modi nemo maxime odio nulla praesentium quam
-        incidunt, debitis, inventore, excepturi aut quibusdam cumque eum
-        accusamus sit.
-      </p>}
+      {close && (
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, sequi?
+          Laboriosam impedit esse modi nemo maxime odio nulla praesentium quam
+          incidunt, debitis, inventore, excepturi aut quibusdam cumque eum
+          accusamus sit.
+        </p>
+      )}
     </div>
   );
 };
