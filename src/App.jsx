02b0e32c53f880af5f7d77f602371c9984fdfd47
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-10-18 19:05:11 +0300

    Add SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import Select from './components/Select/Select';
-
-const countries = [
-  { label: 'United States', value: 'us' },
-  { label: 'Canada', value: 'ca' },
-  { label: 'United Kingdom', value: 'uk' },
-  { label: 'Australia', value: 'au' },
-  { label: 'Germany', value: 'de' },
-  { label: 'France', value: 'fr' },
-  { label: 'Italy', value: 'it' },
-  { label: 'Spain', value: 'es' },
-  { label: 'Japan', value: 'jp' },
-  { label: 'China', value: 'cn' },
-  { label: 'Brazil', value: 'br' },
-  { label: 'India', value: 'in' },
-  { label: 'South Africa', value: 'za' },
-  { label: 'Mexico', value: 'mx' },
-  { label: 'Ukraine', value: 'ua' },
-];
+import TasksList from './components/TasksList/TasksList';
 
 function App() {
   return (
     <>
-      <Select list={countries} />
+      <TasksList />
     </>
   );
 }
