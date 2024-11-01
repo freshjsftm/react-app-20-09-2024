e22322b27873006f08b7f78b25a487ec054d74fe
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-11-01 19:04:32 +0200

    Add localStorage to TasksList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import DragElement from './components/DragElement/DragElement';
+import TasksList from "./components/TasksList/TasksList";
 
 function App() {
   return (
     <>
-      <DragElement text='1' left={325} top={25}/>
-      <DragElement text='2' left={125} top={125}/>
-      <DragElement text='3' left={225} top={225}/>
-      <DragElement />
+      <TasksList />
     </>
   );
 }
--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,22 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Task from '../Task/Task';
 import tasks from './data';
 
 function TasksList() {
+  const [archiv, setArchiv] = useState([]);
   const [dataTasks, setDataTasks] = useState(tasks);
   const setDoneTask = (id) => {
+    let archivTask;
     const newTasks = dataTasks.map((task) => {
       if (task.id === id) {
+        archivTask = { id: task.id, text: task.text };
         return { ...task, isDone: true };
       }
       return task;
     });
     setDataTasks(newTasks);
+    const newArchiv = [...archiv, archivTask];
+    setArchiv(newArchiv);
   };
+  useEffect(() => {
+      const saveArchiv = JSON.parse(localStorage.getItem('archiv'));
+      setArchiv(saveArchiv);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem('archiv', JSON.stringify(archiv));
+  }, [archiv]);
   return (
     <>
       {dataTasks.map((task) => (
         <Task key={task.id} dataTask={task} setDoneTask={setDoneTask} />
+      ))}
+      <hr />
+      <h3>Done task: </h3>
+      {archiv.map((task) => (
+        <p key={task.id}>{task.text}</p>
       ))}
     </>
   );
